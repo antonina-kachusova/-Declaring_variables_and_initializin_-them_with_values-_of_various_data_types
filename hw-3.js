a6ffcf5c7dd41668efbd6dd9d63e984ab2fa3297
antonina-kachusova <168483083+antonina-kachusova@users.noreply.github.com>
2024-09-14 01:10:55 +0300

    Add files via upload

---
+++ hw-3.js
@@ -0,0 +1,78 @@
+// List of variable names:
+// - batterConsistency 
+// - patelniaTemperature 
+// - cookingTime 
+// - flipCount 
+// - ingredientList 
+// - batterVolume 
+// - servingSize 
+// - toppingChoice 
+// - remainingBatter 
+// - patelniaOilLevel 
+
+// 1. Declaring variables and initializing them with values ​​of various data types.
+
+
+let batterConsistency = 'liquid';
+console.log(batterConsistency);
+console.log(typeof batterConsistency);
+
+let patelniaTemperature = 80;
+console.log('patelniaTemperature: ' ,patelniaTemperature);
+console.log(typeof patelniaTemperature);
+
+let cookingTime = 5;
+console.log(cookingTime);
+console.log(typeof cookingTime);
+
+let flipCount = 1;
+console.log(flipCount);
+console.log(typeof flipCount);
+
+const ingredientList = {
+    milk: 1,
+    eggs: 4,
+    flour: 500,
+    oil: 2,
+    sugar: 2,
+    salt: 0.5
+    };
+console.log(ingredientList);
+console.log(typeof ingredientList);
+
+const measurements = {
+    milk: 'l',
+    eggs: 'pc.',
+    flour: 'g',
+    oil: 'tbsp.',
+    sugar: 'tbsp.',
+    salt: 'tsp.'
+    };
+console.log(measurements.milk);
+
+let batterVolume = 450;
+console.log(batterVolume);
+console.log(typeof batterVolume);
+
+let servingSize = {
+    standard: 250,
+    large: 350 
+}
+console.log(servingSize);
+console.log(typeof servingSize);
+
+const toppingChoice = ['meat', 'fruit jam', 'cheese'];
+console.log(toppingChoice);
+console.log(typeof toppingChoice);
+
+let remainingBatter = false; 
+console.log(remainingBatter);
+console.log(typeof remainingBatter);
+
+let patelniaOilLevel 
+console.log(patelniaOilLevel);
+console.log(typeof patelniaOilLevel);
+
+console.log(typeof []); // object
+
+
